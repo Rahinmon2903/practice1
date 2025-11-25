ef991e2336db299e81f2ccc89a4290399902688e
Rahinmon2903 <rahin2903@gmail.com>
2025-11-25 11:18:57 +0530

    dotenv added

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import http from "http"
+import dotenv from "dotenv"
 
-const port=3000;
+dotenv.config();
+
+const port=process.env.PORT
 
 http.createServer((req,res)=>{
     res.writeHead(200,{"content-type":"text/plain"});
-    res.write("welcome to back end");
+    res.write("welcome to back end server");
     res.end();
 
 }).listen(port,()=>{
-    console.log("server is running on port 3000")
+    console.log(`server is running on port ${port}`)
 })
